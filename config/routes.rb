2543af85192019cb4429035f7fc00ef9b581c68e
gotayanagida <gotayanagida5@gmail.com>
2019-08-19 15:59:39 +0900

    ユーザ管理

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  root "users#index" 
-  devise_for :users
+  root "users#index"
+
+  # ログイン、アカウント編集後、任意のページに推移させるための記述
+  devise_for :users, controllers: {
+          registrations: 'users/registrations',
+          sessions: 'users/sessions'
+  }
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # devise_scope :user do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  # end
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(name:"太郎",password:"123456",company:"Ａ株式会社",position:"代表取締役",email:"a@a.com",age:"40",image:open("#{Rails.root}/app/assets/images/pf_1.png"),detail:"よろしくおねがいします。")
+user = User.create(name:"次郎",password:"123456",company:"Ｂ株式会社",position:"営業",email:"b@a.com",age:"30",image:open("#{Rails.root}/app/assets/images/pf_2.png"),detail:"よろしくおねがいします。")
+user = User.create(name:"良子",password:"123456",company:"Ｃ株式会社",position:"広報",email:"c@a.com",age:"25",image:open("#{Rails.root}/app/assets/images/pf_3.png"),detail:"よろしくおねがいします。")
