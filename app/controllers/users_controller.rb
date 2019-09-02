c77be758209997b8bc8bc637e2646db0bf23819c
gotayanagida <gotayanagida5@gmail.com>
2019-09-02 10:49:01 +0900

    search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.search(params[:search])
+    #@users = User.all
   end
 
   # GET /users/1
@@ -68,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :company, :position, :age, :detail, :image, :industry, :hp, :contact, :business)
+      params.require(:user).permit(:name, :company, :position, :age, :detail, :image, :industry, :hp, :contact, :business, :search)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :image, ImageUploader
+
+  def self.search(search)
+    return User.all unless search
+      User.where(['detail LIKE ?', "%#{search}%"])
+      .or(User.where(['industry LIKE ?', "%#{search}%"]))
+      .or(User.where(['name LIKE ?', "%#{search}%"]))
+      .or(User.where(['company LIKE ?', "%#{search}%"]))
+      .or(User.where(['position LIKE ?', "%#{search}%"]))
+      .or(User.where(['business LIKE ?', "%#{search}%"]))
+
+  end
+
 end
